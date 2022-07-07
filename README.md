64bc054c25af11f33df9d94f628d0222e8105861
Omale <omalesamuel98@gmail.com>
2022-07-07 09:20:40 +0000

    Add hover effect to grid

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Sketch Up
 
+Creating Dynamic Elements can be fun until you get caught in the web of the bubble effect (lol)
+
+At some point I was in that web, but now I understand more about event delegation. 
+
+This is my version of the popular Etch A Sketch from The Odin Project.
+
+
+Some stuff I did and learned
+
+- Create dynamic HTML elements with JavaScript
+- Listen to mouse event on the elements that make the grid
+- Remove the grid on page when user changes grid size and recreate the grids with new value
+- Use javascript to add hover effect to grid
+
+
 Live demo >> https://espinovah.github.io/sketch-up/
+
--- app.js
+++ app.js
@@ -7,48 +7,39 @@ const boxes = document.getElementsByClassName('boxGrid');
 const tweakBtn = document.querySelector('button');
 tweakBtn.addEventListener('click', remakeGrid);
 
+
 makeGrid(16);
 function makeGrid (gridSize) {    
-    createRows(gridSize);
     createColumns(gridSize);
-    let box = Array.from(boxes).forEach(div => {
-        div.addEventListener('onmouseover', draw);        
-    });
-    
-    function  draw(e) {
-        e.target.classList.add('drawInk')
-        console.log(e.target.className);
+
+    let sDiv = document.querySelectorAll('.boxGrid');
+    let arr = Array.from(sDiv).forEach(div => div.addEventListener('mouseover', draw));
+}
+
+function createColumns (colNum) {
+    for(let i = 0; i < colNum; i++){
+        let rowDiv = document.createElement('div');
+        rowDiv.className = 'rowGrid';
+        for(let j = 0; j < colNum; j++){
+            let colDiv = document.createElement('div');
+            colDiv.className = 'boxGrid';
+            rowDiv.appendChild(colDiv)
+        }
+        sketchBox.appendChild(rowDiv)
     }
+}
+
+function  draw(e) {
+    this.classList.add('drawInk');
 }
 
 function remakeGrid (){
     let newValue = prompt('Enter Number of Grid');
-    if (newValue > 100) {
-        alert('You can only have 100 sketch grids')
+    if (newValue > 100 || newValue < 1) {
+        alert('SketchUp can only have 1 to 100 grids')
     }
     else {
-    let tweakRow = Array.from(rows).forEach(row => {
-            sketchBox.removeChild(row);
-        })
-
-    makeGrid(newValue);
-    }
-}
-
-
-function createRows (rowNum) {
-    for (let i = 0; i < rowNum; i++) {
-        let rowDiv = document.createElement('div');
-        sketchBox.appendChild(rowDiv).className = 'rowGrid';
-    } 
-}
-
-function createColumns (colNum) {
-    for(let i = 0; i < rows.length; i++){
-        for(let j = 0; j < colNum; j++){
-            let colDiv = document.createElement('div');
-            rows[j].appendChild(colDiv).className = 'boxGrid';
-        }
-
+        let tweakRow = Array.from(rows).forEach(row => sketchBox.removeChild(row))
+        makeGrid(newValue);
     }
 }
