9f4cc595a21ac0ef2b3340ae6780ef2ca2da8086
Omale <omalesamuel98@gmail.com>
2022-08-15 18:45:36 +0000

    Add clear button

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 const sketchBox = document.querySelector('.sketchPanel');
 const rows = document.getElementsByClassName('rowGrid');
 
-const boxes = document.getElementsByClassName('boxGrid');
-
+let isColored = false;
 
 const tweakBtn = document.querySelector('button');
 tweakBtn.addEventListener('click', remakeGrid);
 
+const clearBtn = document.querySelector('.clearGrid');
+clearBtn.addEventListener('click', clearGrid);
+
 
 makeGrid(16);
-function makeGrid (gridSize) {    
+function makeGrid(gridSize) {
     createColumns(gridSize);
 
     let sDiv = document.querySelectorAll('.boxGrid');
     let arr = Array.from(sDiv).forEach(div => div.addEventListener('mouseover', draw));
 }
 
-function createColumns (colNum) {
-    for(let i = 0; i < colNum; i++){
+function createColumns(colNum) {
+    for (let i = 0; i < colNum; i++) {
         let rowDiv = document.createElement('div');
         rowDiv.className = 'rowGrid';
-        for(let j = 0; j < colNum; j++){
+        for (let j = 0; j < colNum; j++) {
             let colDiv = document.createElement('div');
             colDiv.className = 'boxGrid';
             rowDiv.appendChild(colDiv)
@@ -29,13 +31,23 @@ function createColumns (colNum) {
     }
 }
 
-function  draw(e) {
+function draw(e) {
     this.classList.add('drawInk');
+    isColored = true;
 }
 
-function remakeGrid (){
+function clearGrid() {
+    if (isColored) {
+        const grid = document.getElementsByClassName("drawInk");
+        console.log(grid);
+        gridDiv = Array.from(grid).forEach(div => div.classList.remove('drawInk'));
+        isColored = false;
+    } return;
+}
+
+function remakeGrid() {
     let newValue = prompt('Enter Number of Grid');
-    if ((newValue%1) != 0){
+    if ((newValue % 1) != 0) {
         alert('You can only enter numbers')
     }
     else if (newValue > 100 || newValue < 1) {
