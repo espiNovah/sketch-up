2b5b1b06b00bee9796030e2062da336800cfa2b2
Omale <omalesamuel98@gmail.com>
2022-08-16 05:52:36 +0000

    Add toggle switch and random RGB colors

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const sketchBox = document.querySelector('.sketchPanel');
 const rows = document.getElementsByClassName('rowGrid');
 
-let isColored = false;
 
 const tweakBtn = document.querySelector('button');
 tweakBtn.addEventListener('click', remakeGrid);
@@ -9,11 +8,14 @@ tweakBtn.addEventListener('click', remakeGrid);
 const clearBtn = document.querySelector('.clearGrid');
 clearBtn.addEventListener('click', clearGrid);
 
+const rbwCheck = document.getElementById('rbwSwitch');
+rbwCheck.addEventListener('click', addRainbowColor);
+
+let isColored = false;
 
 makeGrid(16);
 function makeGrid(gridSize) {
     createColumns(gridSize);
-
     let sDiv = document.querySelectorAll('.boxGrid');
     let arr = Array.from(sDiv).forEach(div => div.addEventListener('mouseover', draw));
 }
@@ -32,17 +34,29 @@ function createColumns(colNum) {
 }
 
 function draw(e) {
-    this.classList.add('drawInk');
+    if (rbwCheck.checked == true) {
+        this.style.backgroundColor = addRainbowColor();
+    } else {
+        this.style.backgroundColor = '#000000'
+    }
     isColored = true;
+}
+
+function addRainbowColor() {
+    const randomRGB = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+    const r = randomRGB(0, 255);
+    const g = randomRGB(0, 255);
+    const b = randomRGB(0, 255);
+    const rgb = `rgb(${r},${g},${b})`;
+    return rgb;
 }
 
 function clearGrid() {
     if (isColored) {
-        const grid = document.getElementsByClassName("drawInk");
-        console.log(grid);
-        gridDiv = Array.from(grid).forEach(div => div.classList.remove('drawInk'));
+        const grid = document.getElementsByClassName("boxGrid");
+        gridDiv = Array.from(grid).forEach(div => div.style.backgroundColor = null);
         isColored = false;
-    } return;
+    }
 }
 
 function remakeGrid() {
@@ -54,7 +68,7 @@ function remakeGrid() {
         alert('SketchUp can only have 1 to 100 grids')
     }
     else {
-        let tweakRow = Array.from(rows).forEach(row => sketchBox.removeChild(row))
+        clearGrid();
         makeGrid(newValue);
     }
 }
